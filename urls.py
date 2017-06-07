ec41bb136cae00532cfb9da745492cd66bc66b52
miwooro <miwooro@hotmail.com>
2017-06-07 21:17:50 +0800

    change channel url to index

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    # url(r'^$', views.channel, name='channel'),
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<slug>[\w-]+)/$', views.channel, name='channel'),
+    url(r'^$', views.channel, name='channel'),
+    # url(r'^$', views.index, name='index'),
+    # url(r'^(?P<slug>[\w-]+)/$', views.channel, name='channel'),
 ]
