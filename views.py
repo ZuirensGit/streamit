1c0040e86b4880dbece0f656e03cb0a10e7f4a39
miwooro <miwooro@hotmail.com>
2017-06-07 21:11:08 +0800

    change channel url to index

--- views.py
+++ views.py
@@ -9,8 +9,9 @@ def index(request):
     channel = Channel.objects.all()[0]
     return HttpResponseRedirect('{}/'.format(channel.slug))
 
-def channel(request, slug):
-    channel = get_object_or_404(Channel, slug=slug)
+
+def channel(request):
+    channel = Channel.objects.all()[0]
     try:
         control_meta = ControlMeta.objects.filter(on_air=True, end_time__gt=timezone.localtime(timezone.now())).order_by('end_time')[0]
     except:
@@ -27,3 +28,23 @@ def channel(request, slug):
     }
 
     return render(request, 'index.html', context)
+
+#
+# def channel(request, slug):
+#     channel = get_object_or_404(Channel, slug=slug)
+#     try:
+#         control_meta = ControlMeta.objects.filter(on_air=True, end_time__gt=timezone.localtime(timezone.now())).order_by('end_time')[0]
+#     except:
+#         raise Http404("錯誤")
+#     performers = Performer.objects.filter(channel=channel, start_time__gt=timezone.localtime(timezone.now())).exclude(pk=control_meta.performer.pk).order_by('start_time')[0:4]
+#     sponsors = Sponsor.objects.all()
+#
+#
+#     context = {
+#         'channel': channel,
+#         'control_meta': control_meta,
+#         'performers': performers,
+#         'sponsors': sponsors,
+#     }
+#
+#     return render(request, 'index.html', context)
